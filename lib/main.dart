5d90d92bf8c230b8470067a8e5fc69f8b91d9e11
Jhainey Perez <jhainey.perez@dropi.co>
2024-10-14 12:01:40 -0500

    envuelvo el main witget en el provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,16 +10,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tok Tik',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tok Tik'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => DiscoverProvider())],
+      child: MaterialApp(
+        title: 'Tok Tik',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Tok Tik'),
+          ),
+          body: const Center(
+            child: Text('Hello World'),
+          ),
         ),
       ),
     );
